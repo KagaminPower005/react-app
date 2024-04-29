8c5f31009afe4a8f7ba35fcd6f451b17a7bda652
kagamin8078 <kagamin8078@proton.me>
2024-04-29 10:21:37 +0900

    2024/4/29 フロントエンド側のコードをGitHubにアップする

--- src/index.js
+++ src/index.js
@@ -7,11 +7,34 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <nav className="navbar fixed-top navbar-expand navbar-light bg-light">
+    <ul style={{width:"100%"}} className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <a className="nav-link" href="/">top</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/post">post</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/goods">good</a>
+      </li>
+    </ul>
+    </nav>
+
+    <div className="container">
+      
+      <App />
+
+      <div className="my-3 text-center">
+        <span className="font-weight-bold">
+          <a href="/admin/logout?next=/sns/">
+            [ logout ]</a></span>
+        <span className="float-right">copyright 2023 
+          SYODA-Tuyano.</span>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
